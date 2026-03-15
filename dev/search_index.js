2429555639feebb98615ac75da0693bb99e302d2
Documenter.jl <documenter@juliadocs.github.io>
2026-03-15 17:46:42 +0000

    build based on 3a9d605

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MultiObjectiveDescent","page":"Home","title":"MultiObjectiveDescent","text":"Documentation for MultiObjectiveDescent.\n\n","category":"section"}]
+}
